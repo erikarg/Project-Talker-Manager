ee0b666f9e40bff052715e1d1da2e83bf13bb8fc
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-10-04 20:17:46 -0300

    Adiciona requisito 4 corrigido

--- src/middlewares/validateEmail.js
+++ src/middlewares/validateEmail.js
@@ -1,14 +1,16 @@
+/* eslint-disable no-useless-escape */
 function validateEmail(req, res, next) {
   const { email } = req.body;
   const regex = /^[a-zA-Z0-9_.+]+(?<!^[0-9]*)@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-  const emailFormat = email.match(regex);
-
+  
   if (!email) {
-    return res.status(400).json({ message: 'O campo \'email\' é obrigatório' });
+    return res.status(400).json({ message: 'O campo \"email\" é obrigatório' });
   }
-
+  
+  const emailFormat = email.match(regex);
   if (!emailFormat) {
-    return res.status(400).json({ message: 'O \'email\' deve ter o formato \'email@email.com\'' });
+    return res.status(400).json({
+      message: 'O \"email\" deve ter o formato \"email@email.com\"' });
   }
   next();
 }
