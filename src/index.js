6069fbcd487eab52dc399c35612fa0cb6ad1790b
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-10-04 15:13:27 -0300

    Adiciona requisito 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,7 +9,6 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// edit só pro primeiro commit
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -16,3 +17,15 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+const pathTalker = path.resolve(
+  __dirname,
+  '..',
+  'src',
+  'talker.json'
+);
+
+app.get('/talker', async (req, res) => {
+  const talker = JSON.parse(await fs.readFile(pathTalker, "utf8"));
+  return res.status(200).json(talker);
+})
