b697590052a20db8f1b2720139ee915cd3a9e326
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-10-04 14:24:40 -0300

    Primeiro commit. Bora!

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+// edit só pro primeiro commit
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
